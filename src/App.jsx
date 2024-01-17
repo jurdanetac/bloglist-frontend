b4c86aa448c9eab9333b4c31a1f566c17fcef114
Juan Urdaneta <jurdanetac10@gmail.com>
2024-01-17 13:43:58 -0400

    Refactor Blogs component into its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blogs from "./components/Blogs";
+import blogService from "./services/blogs";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+  return <Blogs blogs={blogs} />;
+};
 
-export default App
+export default App;
